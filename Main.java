3fd642239d68f820a25469ab0513cc2da243ced4
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-18 22:27:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 100;
+        double value = 10.5;
+        boolean flag = true;
+
+        System.out.println(String.valueOf(num)); 
+        System.out.println(String.valueOf(value)); 
+        System.out.println(String.valueOf(flag)); 
+    }
+}
